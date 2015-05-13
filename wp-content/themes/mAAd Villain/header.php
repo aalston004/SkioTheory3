10d80ec7b8b07755f7477b9144a2ca84c022e645
aalston004 <aalston004@gmail.com>
2015-05-13 00:01:11 -0400

    Switched back to bs, tried some tweaks in header

--- wp-content/themes/mAAd Villain/header.php
+++ wp-content/themes/mAAd Villain/header.php
@@ -9,7 +9,7 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <title><?php wp_title(' | ', TRUE, 'right'); ?></title>
+<title><?php wp_title(' | ', TRUE, 'right'); ?></title>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
@@ -22,11 +22,11 @@
 <div id="page" class="hfeed site">
     <header>
 			<nav id="site-navigation" class="main-navigation" role="navigation">
-			    <button class="menu-toggle"><?php _e( 'Primary Menu', 'maad-villain' ); ?></button>
+			    <h1 class="menu-toggle"><?php _e( 'Primary Menu', 'maad-villain' ); ?></h1>
                 <a class="skip-link screen-reader-text" href="#content"><?php _e('Skip to Content','maad-villain'); ?></a>
 
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+			    <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+		    </nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
